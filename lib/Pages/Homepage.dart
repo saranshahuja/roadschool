5063691ec15f88ecdde34ace3220801b8cb882e8
saranshahuja <saranshahuja614@gmail.com>
2023-08-09 10:56:39 -0700

    CHanged HomePage

--- lib/Pages/Homepage.dart
+++ lib/Pages/Homepage.dart
@@ -6,7 +6,15 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title: Text("something",style: TextStyle(color: Colors.black),), backgroundColor: Colors.grey,),
+      body: Container(
 
+        child: Column(
+          children: [
+
+          ],
+        ),
+      ),
 
     );
   }
---
+++ lib/Pages/learnChapters.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+
+
+class learnChapters extends StatefulWidget {
+  const learnChapters({super.key});
+
+  @override
+  State<learnChapters> createState() => _learnChaptersState();
+}
+
+class _learnChaptersState extends State<learnChapters> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+
+      ),
+    );
+  }
+}
