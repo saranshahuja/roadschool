712efb9497405f3647bf3c00373bda7b340c8dd0
Vaibhav003-beep <Vaibhav.Datta@student.ufv.ca>
2023-08-15 01:38:22 +0530

    Completed the learn chapters screen having some issues need help from one of the mentor.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:roadschool/Pages/Homepage.dart';
 // import 'package:firebase_core/firebase_core.dart';
+import 'Pages/SplashScreen.dart';
+import 'Pages/learnChapters.dart';
 import 'Pages/progressScreen.dart';
 import 'firebase_options.dart';
 
@@ -25,7 +27,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: ProgressScreen(),
+      home: SplashScreenpage(),
     );
   }
 }
