5f43d53b7d3b74d61757cd3436e63d95956c4f6c
Saumyaa <saumyaaufv@gmail.com>
2023-08-17 23:41:30 +0530

    Graph Package

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:roadschool/Pages/Homepage.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'Pages/SplashScreen.dart';
-import 'Pages/Practicetests.dart';
+import 'Pages/learnChapters.dart';
 import 'Pages/progressScreen.dart';
-import 'Pages/testPages.dart';
 import 'firebase_options.dart';
 
 
@@ -28,7 +27,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: LinearProgressIndicatorApp(),
+      home: learnChapterpage(),
     );
   }
 }
