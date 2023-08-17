bf1b9b4cc09c97cec79c34a0429eb07f1ce5e289
Vaibhav003-beep <Vaibhav.Datta@student.ufv.ca>
2023-08-18 01:42:50 +0530

    Linked the sheets.

--- lib/Pages/Homepage.dart
+++ lib/Pages/Homepage.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:roadschool/Pages/Practicetests.dart';
 import 'package:roadschool/Pages/learnChapters.dart';
+import 'package:roadschool/Pages/progressScreen.dart';
+
+import 'SplashScreen.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -9,8 +12,20 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          backgroundColor: Color(0xFFD9D9D9),
-          leading: Image.asset('assets/images/iconwtbg.png', width: 40, height: 40), // Adjust width and height
+        backgroundColor: Color(0xFFD9D9D9),
+        leading: IconButton(
+          icon: Image.asset(
+            'assets/images/iconwtbg.png',
+            width: 40,
+            height: 40,
+          ),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SplashScreenpage()), // Replace with the appropriate page
+            );
+          },
+        ),
       ),
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: Color(0xFF034D91),
@@ -117,6 +132,10 @@ class HomePage extends StatelessWidget {
                     height: 110,
                     child: ElevatedButton(
                       onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => progressScreen()), // Navigate to HomePage
+                        );
                         // Handle Course Progress button press
                       },
                       style: ElevatedButton.styleFrom(
