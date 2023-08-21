ce5ebffa7e0d76b580f8e0f53822cd99693e44c3
Saumyaa <saumyaaufv@gmail.com>
2023-08-22 00:19:07 +0530

    Settings 2nd Page - With bottom nav bar and buttons. Unable to centrally align the boxes vertically. Another issue: Box size is not same. Need help from mentor

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:roadschool/Pages/Homepage.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:sizer/sizer.dart';
+import 'package:roadschool/Pages/settings2.dart';
 import 'Pages/AnswerScreen.dart';
 import 'Pages/Practicetests.dart';
 import 'Pages/SplashScreen.dart';
 // import 'Pages/learnChapters.dart';
-import 'Pages/chapterReveiwScreen.dart';
 import 'Pages/progressScreen.dart';
 import 'Pages/settings.dart';
 import 'Pages/testPages.dart';
 import 'firebase_options.dart';
 import 'package:fl_chart/fl_chart.dart';
 
-main() {
+
+
+ main() {
 //   await Firebase.initializeApp(
 // options: DefaultFirebaseOptions.currentPlatform,
 // );
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Sizer(
-        builder: (context, orientation, deviceType) {
-      return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: SettingsApp(),
-      );
-    });
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Settingstwo(),
+    );
   }
 }
